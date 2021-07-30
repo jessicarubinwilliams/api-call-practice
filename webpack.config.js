7dd5cdbd05501f8bc6b9925a1988ce19ffe8e762
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-07-29 21:10:46 -0700

    Customize environment to be project specific & fix vulnerabilites

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Document',
+      title: 'Weather',
       template: './src/index.html',
       inject: 'body'
     }),
