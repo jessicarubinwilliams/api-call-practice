da57779470ea8ca1a4f0f1a6bcee1f765e72d069
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-09 14:03:33 -0700

    Switch API call method to async functions using await keyword

--- src/index.js
+++ src/index.js
@@ -16,15 +16,17 @@ function getElements(response) {
     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
     $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
   } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
+    $('.showErrors').text(`There was an error: ${response}`);
   }
+}
+
+async function makeApiCall(city) {
+  const response = await WeatherService.getWeather(city);
+  getElements(response);
 }
 
 $('#weatherLocation').click(function() {
   let city = $("#location").val();
   clearFields();
-  WeatherService.getWeather(city)
-    .then(function(response) {
-      getElements(response);
-    });
+  makeApiCall(city);
 });
